9391d42769ca830423dd358f4716e8d20ad94a78
Furkan Kaymaz <furkannkaymaz@gmail.com>
2022-09-19 21:52:59 +0300

    customtopcontent added

---
+++ app/src/main/java/com/commerce/custom_layouts/CustomTopContent.kt
@@ -0,0 +1,19 @@
+package com.commerce.custom_layouts
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import com.commerce.base.BaseLinearLayout
+import com.commerce.databinding.CustomTopcontentBinding
+
+class CustomTopContent(context: Context, attrs: AttributeSet? = null) :
+    BaseLinearLayout<CustomTopcontentBinding>(context, attrs) {
+
+    override fun createView(inflater: LayoutInflater): CustomTopcontentBinding {
+        return CustomTopcontentBinding.inflate(inflater, this, true)
+    }
+
+    override fun viewCreated(attrs: AttributeSet?) {
+
+    }
+}
