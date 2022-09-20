af982d250e923a6a2191c7a4494dddb71f22703c
Furkan Kaymaz <furkannkaymaz@gmail.com>
2022-09-20 23:41:46 +0300

    added new values

--- app/src/main/java/com/commerce/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/commerce/ui/home/view/HomeFragment.kt
@@ -1,22 +1,23 @@
 package com.commerce.ui.home.view
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import com.commerce.R
 import com.commerce.base.BaseFragment
 import com.commerce.databinding.FragmentHomeBinding
 import com.commerce.ui.home.viewmodel.HomeViewModel
+import com.commerce.utils.extension.toast
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class HomeFragment : BaseFragment<FragmentHomeBinding,HomeViewModel>(){
+class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
 
     override val viewModel by viewModels<HomeViewModel>()
 
     override fun configureUiItems() {
-        binding?.tbDate?.newTab()?.setText("Önceki Gün \n 1.450 TL")?.let { binding?.tbDate?.addTab(it) };
-        binding?.tbDate?.newTab()?.setText("Önceki Gün \n 1.450 TL")?.let { binding?.tbDate?.addTab(it) };
-        binding?.tbDate?.newTab()?.setText("Önceki Gün \n 1.450 TL")?.let { binding?.tbDate?.addTab(it) };
+        tabLayoutConfigure()
 
     }
 
@@ -24,6 +25,26 @@ class HomeFragment : BaseFragment<FragmentHomeBinding,HomeViewModel>(){
         viewModel.getData(requireActivity())
 
     }
+
+    override fun observerData() {
+        super.observerData()
+        viewModel.getData.observe(viewLifecycleOwner) {
+            Log.d("data1213", it?.data.toString())
+        }
+        viewModel.error.observe(viewLifecycleOwner) {
+            context?.toast(getString(R.string.unExpectedError))
+        }
+    }
+
+    fun tabLayoutConfigure() {
+        binding?.tbDate?.newTab()?.setText("Önceki Gün \n 1.450 TL")
+            ?.let { binding?.tbDate?.addTab(it) };
+        binding?.tbDate?.newTab()?.setText("28 Haz Sal \n 1.450 TL")
+            ?.let { binding?.tbDate?.addTab(it) };
+        binding?.tbDate?.newTab()?.setText("Sonraki Gün \n 1.450 TL")
+            ?.let { binding?.tbDate?.addTab(it) };
+    }
+
     override fun layoutResource(
         inflater: LayoutInflater,
         container: ViewGroup?
---
+++ app/src/main/java/com/commerce/utils/extension/ToastExt.kt
@@ -0,0 +1,7 @@
+package com.commerce.utils.extension
+
+import android.content.Context
+import android.widget.Toast
+
+fun Context.toast(message: CharSequence) =
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
