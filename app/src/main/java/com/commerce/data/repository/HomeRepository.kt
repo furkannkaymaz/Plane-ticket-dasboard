139731c09e6aa2bd6bb421de50c08192bcbd698a
Furkan Kaymaz <furkannkaymaz@gmail.com>
2022-09-21 23:26:20 +0300

    code review

--- app/src/main/java/com/commerce/data/repository/HomeRepository.kt
+++ app/src/main/java/com/commerce/data/repository/HomeRepository.kt
@@ -1,9 +1,7 @@
 package com.commerce.data.repository
 
 import android.content.Context
-import android.content.res.Resources
 import com.commerce.R
-import com.commerce.base.BaseRepository
 import com.commerce.data.model.FlightModel
 import com.google.gson.Gson
 import javax.inject.Inject
@@ -14,6 +12,5 @@ class HomeRepository @Inject constructor(){
         val response = context.resources.openRawResource(R.raw.enuygun_flight_search).bufferedReader().use { it.readText() }
         return Gson().fromJson(response, FlightModel:: class.java)
     }
-
 }
 
